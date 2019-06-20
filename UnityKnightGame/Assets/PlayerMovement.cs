16d76217270f85e97118689bf192d3b2c2f606ab
BrandonJLei <brandonlei135@gmail.com>
2019-06-20 16:38:08 -0700

    Added Movement to character
    Character can now move left/right, jump and crouch but no animations are added yet

---
+++ UnityKnightGame/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jumpBool = false;
+    bool crouchBool = false;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpBool = true;
+        }
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouchBool = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouchBool = false;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouchBool, jumpBool);
+        jumpBool = false;
+ 
+
+        
+    }
+}
